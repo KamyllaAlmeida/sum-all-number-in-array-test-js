b38d41cc9766d98d3b9c5da6c5c854f9c6ba7f8b
KamyllaAlmeida <kamylla.florencio@gmail.com>
2018-11-22 04:47:14 +0000

    sum array recursive

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  
+  if(!(Array.isArray(array))) {
+    if(isNaN(array)){
+      return 0;
+    }
+    return array;
+  }
+
+  if(array.length < 1) {
+    return 0;    
+  }
+  if(array.length === 1) {
+    if(Array.isArray(array[0])) {
+      return sumItems(array[0]);
+    }
+    
+    return array[0];    
+  }
+  return sumItems(array[0]) + sumItems(array.splice(1));
 }
+  
+   
+  
+  
+  
+    
 
 module.exports = sumItems;
